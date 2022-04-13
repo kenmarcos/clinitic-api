aa7149c71c3b68db3ce3be6ba2f4cc998172da04
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2022-04-13 00:52:38 -0300

    feat: add cpf column in patients table

--- patients/models.py
+++ patients/models.py
@@ -11,5 +11,6 @@ class Patient(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
+    cpf = models.CharField(max_length=11)
     sex = models.CharField(max_length=1, choices=SEX_OPTIONS)
     birth_date = models.DateField()
