cb157e1d4b68d9bdba4e7f060d543c97f9aa64c4
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2022-04-12 22:50:23 -0300

    feat: create patients and appointments tables

---
+++ appointments/models.py
@@ -0,0 +1,16 @@
+from tkinter import CASCADE
+from django.db import models
+import uuid
+
+
+class Appointment(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    description = models.TextField(null=True)
+    isActive = models.BooleanField(default=True)
+
+    doctor = models.ForeignKey('doctors.Doctor', on_delete=models.CASCADE, related_name='appointments')
+    patient = models.ForeignKey('patients.Patient', on_delete=models.CASCADE, related_name='appointments')
---
+++ patients/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+import uuid
+
+
+class Patient(models.Model):
+
+    SEX_OPTIONS = (
+        ('F', 'Female'),
+        ('M', 'Male'),
+    )
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    sex = models.CharField(max_length=1, choices=SEX_OPTIONS)
+    birth_date = models.DateField()
