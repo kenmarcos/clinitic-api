cb1486b4510e55b16b4e5ebf349a81e40ba5bc3b
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2022-04-15 23:20:58 -0300

    chore: adjust CORS

--- clinitic_api/settings.py
+++ clinitic_api/settings.py
@@ -140,7 +140,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'doctors.Doctor'
 
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-]
+CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_WHITELIST = [
+#     'http://localhost:3000',
+# ]
